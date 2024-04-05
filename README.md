dc9074657469f2b4162c05a258c3897a6165d036
andrew.robertson1 <andrew.robertson@enablo.com>
2024-04-05 14:28:47 +1100

    wip

--- README.md
+++
@@ -1,5 +0,0 @@
-# wordpress-integration
-
-Builds Markup to be used by a WordPress website.
-
-Directus data: https://0r78b6qe.directus.app/
--- src/types/Enums.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace Enums {
-  export type PersonGenderEnum = 'Male' | 'Female' | 'Unknown';
-  export type TeamGenderEnum = 'Male' | 'Female' | 'Mixed';
-  export type PrivacyEnum = 'Display Both' | 'Display Phone Only' | 'Display Email Only' | 'Display None';
-}
